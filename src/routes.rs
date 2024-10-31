9d2b0f5997b4f89cf7e94a93319811792eb6cbbe
val213 <1042594899@qq.com>
2024-11-01 01:34:55 +0800

    updates router

--- src/routes.rs
+++ src/routes.rs
@@ -2,7 +2,7 @@ use actix_web::web;
 use crate::controller::book_controller::{list_books, get_book_details, recent_books, top_rated_books};
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/books")
+        web::scope("/api/libre/v1/books")
             .route("/list", web::post().to(list_books))
             .route("/details/{book_id}", web::get().to(get_book_details))
             .route("/recent", web::get().to(recent_books))
